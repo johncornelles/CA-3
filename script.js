7f788ddda9620c48b66f8f8f497acc348ff9e1f3
johncornelles <john.cornellews@kalvium.community>
2023-12-20 17:43:21 +0530

    update 2

--- script.js
+++ script.js
@@ -6,13 +6,16 @@ const searchIconDiv = document.querySelector('.searchIcondiv');
 const foodGenieTitle = document.getElementById('foodgenieTitle');
 const foodRandomImage = document.querySelector('.foodRandom');
 const detailsRandom = document.querySelector('.detailsRandom');
-const randomPre = document.querySelector('.randomPre');
 let surpriseButton;
-const ingredientsButton = document.querySelector('.IngredientsRandom');
-const allRandomButtons = document.querySelectorAll('.btn-randoms');
+let ingredientsButtonSearch;
+const ingredientsButton = document.querySelector('.IngredientsRandombtn');
+const allRandomButtons = document.querySelectorAll('.btn');
 const genie = document.querySelector('.genie');
 const container = document.querySelector('.container');
-const randomIngredients = document.querySelector('.randomIngredients')
+const randomIngredients = document.querySelector('.randomIngredients');
+const resultsDiv = document.querySelector('.results');
+const ingredientsDiv = document.querySelector('.ingredients');
+
 const randomrefresher = async () => {
   try {
     let res = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
@@ -23,7 +26,7 @@ const randomrefresher = async () => {
   }
 }
 
-const handler = async () => {
+const randomHandler = async () => {
   try {
     let data = await randomrefresher();
     let mealdata = data.meals[0];
@@ -43,38 +46,111 @@ const handler = async () => {
           <button class="surprise btn">Surprise Me!</button>   <button class="IngredientsRandombtn btn">Ingredients</button>
         </pre>
       </div>`;
-    console.log(mealdata)
     genie.innerHTML = returner;
-    surpriseButton = document.querySelector('.surprise'); // Reassign surpriseButton each time
-    let IngredientsArr = `<pre> \n <b>Ingredients</b> \n`
+    surpriseButton = document.querySelector('.surprise');
+    let IngredientsArr = `<pre> <b>${mealdata.strMeal}</b>\n \n<b>Ingredients</b> \n`
     for (let i in mealdata) {
         if (i.includes('strIngredient') && mealdata[i] != '') IngredientsArr += `\n  ${mealdata[i]} \n`
     }
     IngredientsArr += `\n \n <button class="close btn">Close</button> <a href="${mealdata.strSource}" target="_blank" class="recipe btn">Recipe</a> </pre>`
-    console.log(IngredientsArr) 
     randomIngredients.innerHTML = IngredientsArr
-
   } catch (error) {
     console.error(error);
   }
 };
 
+const categoryFetcher = async (query) => {
+  try {
+    let data = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${query}`)
+    let mealdata = await data.json()
+    return mealdata
+  } catch (error) {
+    console.error(error)
+  }
+}
+const searchDatafetcher = async (id) => {
+  let data = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`)
+  let meal = await data.json()
+  return meal
+}
+
+const displayIngredients = (data) => {
+            let fragment = `<pre>\n<b>${data.strMeal}</b>\n\n<b>Ingredients</b>\n`;
+            // console.log(data, '79')
+            for (let i = 1; i <= 20; i++) {
+                const ingredient = data[`strIngredient${i}`];
+                const measure = data[`strMeasure${i}`];
+
+                if (ingredient && measure) 
+                    fragment += `\n ${measure} ${ingredient} \n`;
+                
+            }
+
+            fragment += `\n<button class="close btn">Close</button>    <a href="${data.strSource}" target="_blank" class="recipe btn">Recipe</a></pre>`;
+            ingredientsDiv.innerHTML = fragment
+            ingredientsDiv.style.display = 'block';
+
+            const closeBtn = ingredientsDiv.querySelector('.close');
+            closeBtn.onclick = () => ingredientsDiv.style.display = 'none'
+        };
+
+const searchHandler = async (query) => {
+    let mealdata = await categoryFetcher(query);
+    let meals = mealdata.meals;
+    meals = meals.splice(0, 10)
+
+    for (let meal of meals) {
+        let x = await searchDatafetcher(meal.idMeal);
+        let data = x.meals[0];
+        let i = 1
+        let fragment = `
+            <div class="item${data.idMeal} card">
+                <img src="${data.strMealThumb}" height="200px" width="200px" alt="random food">
+                <pre>
+                
+                    <b>${data.strMeal}</b>
+
+                    ${data.strCategory}
+
+                    ${data.strArea}stringify
+
+
+                    <a href="${data.strSource}" target="_blank" class="btn recipe">Recipe</a> <button class="btn btn-ingredients-search ingredientsbtn${data.idMeal} ${data.idMeal}">Ingredients</button>
+                </pre>
+                    
+            </div>`;
+        resultsDiv.innerHTML += fragment;
+        console.log(meal)
+        console.log('data', data)
+        resultsDiv.addEventListener('click', (e) => {
+        if(e.target.classList.contains(`${data.idMeal}`)){
+          displayIngredients(data)
+        }
+})
+    }
+    // document.querySelectorAll('.btn-ingredients-search').forEach((btn) => {
+    //     let x = btn.getAttribute('data-mealdata');
+    //     // let y = JSON.parse(x)
+    //     console.log(x)
+    // })
+    // document.querySelectorAll('.btn-ingredients-search').forEach((btn) => {
+    //     btn.onclick = () => {console.log(btn.getAttribute('data-mealdata'))}
+    // })
+
+};
+
 window.onload = async () => {
-  await handler(); // Call handler initially
+  await randomHandler();
 
   container.onclick = (e) => {
-    if(e.target.classList.contains('surprise')) handler()
+    if(e.target.classList.contains('surprise')) randomHandler()
     if (e.target.classList.contains('IngredientsRandombtn'))
     randomIngredients.style.display = 'block';
   }
 };
 
-const closeBtn = document.querySelector('.close');
-
 randomIngredients.onclick = (e) => {
   if (e.target.classList.contains('close')) randomIngredients.style.display = 'none'; 
 };
 
-randomIngredients.onclick = (e) => {
-  if(e.target.classList.contains('close')) randomIngredients.style.display = 'none'
-}
+queryInput.addEventListener('keypress', (e) => {if(e.key == 'Enter' && queryInput.value.trim() != '') searchHandler(queryInput.value)})
